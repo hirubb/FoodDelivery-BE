329efd0e3193e0ef69a0a449ad8411d94610fe29
kavindu gayashan <127669394+Gaya2001@users.noreply.github.com>
2025-03-29 17:42:17 +0530

    Delivery Service node modules are removed

---
+++ Delivery-Service/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const DB_URI = process.env.MONGO_URI;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose
+    .connect(DB_URI)
+    .then(() => console.log("✅ Connected to MongoDB"))
+    .catch((err) => console.error("❌ Could not connect to MongoDB:", err));
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`\n🚀 Server running at http://localhost:${PORT}`);
+});
+
+
+
+MONGO_URI = mongodb + srv://hirunbhathiya:hirun123@food-delivery.rugwm.mongodb.net/
+PORT = 3001
