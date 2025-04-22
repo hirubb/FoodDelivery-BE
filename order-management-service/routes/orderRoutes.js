c314fe8c91e53f0298c4c19164cf259647dc2f33
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-04-22 22:20:36 +0530

    deleted node modules

---
+++ order-management-service/routes/orderRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { placeOrder ,updateOrderStatus,getOrderStatus} = require("../controllers/orderController.js");
+
+const auth = require('../middleware/authMiddleware.js');
+
+// Protected route - only authenticated users can place orders
+router.post("/", auth, placeOrder);
+
+// Route to update order status
+router.patch("/:id/status", updateOrderStatus);
+
+// Route to get order status
+router.get("/:id/status", getOrderStatus);
+
+module.exports = router;
