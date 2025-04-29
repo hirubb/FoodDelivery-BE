ae39d29b6556751b271dec973a2665abd8431c2c
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-04-29 13:52:51 +0530

    menu items image upload

--- Restaurant-Service/routes/menuItemRoutes.js
+++ Restaurant-Service/routes/menuItemRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const menuItemController = require("../controllers/menuItemController");
 const uploadMenuItemImages = require("../middleware/menuItemUpload");
+const upload = require("../middleware/upload");
 
 // Routes for MenuItem
 router.post("/create", uploadMenuItemImages,menuItemController.createMenuItem); // Add a menu item
 router.get("/:menuId", menuItemController.getMenuItemsByMenu); // Get menu items by menu
-router.put("/:menuItemId", menuItemController.updateMenuItem); // Update menu item
+router.put("/:menuItemId",uploadMenuItemImages, menuItemController.updateMenuItem); // Update menu item
 router.delete("/:menuItemId", menuItemController.deleteMenuItem); // Delete menu item
 
 module.exports = router;
